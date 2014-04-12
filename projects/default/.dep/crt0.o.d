a593e6ddbaaf5e0669e6b3fc7164c15537164e80
Muhaimin Ahsan <leroux@fezrev.com>
2014-04-11 23:45:13 -0500

    Remove binary and temp files.

--- projects/default/.dep/crt0.o.d
+++
@@ -1,7 +0,0 @@
-bin/obj/crt0.o: ../../vexell/../ChibiOS-RT/os/ports/GCC/ARMCMx/crt0.c \
- /Users/leroux/src/gcc-arm-none-eabi-4_8-2013q4/lib/gcc/arm-none-eabi/4.8.3/include/stdint.h \
- /Users/leroux/src/gcc-arm-none-eabi-4_8-2013q4/lib/gcc/arm-none-eabi/4.8.3/include/stdint-gcc.h
-
-/Users/leroux/src/gcc-arm-none-eabi-4_8-2013q4/lib/gcc/arm-none-eabi/4.8.3/include/stdint.h:
-
-/Users/leroux/src/gcc-arm-none-eabi-4_8-2013q4/lib/gcc/arm-none-eabi/4.8.3/include/stdint-gcc.h:
