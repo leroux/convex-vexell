465f8c90df58b3e23827b38bac2e344e308b7f38
Muhaimin Ahsan <leroux@fezrev.com>
2014-04-14 16:27:12 -0500

    Forgot to commits ChibiOS.Threads.

---
+++ vexell/src/ChibiOS/Threads.hs
@@ -0,0 +1,9 @@
+module ChibiOS.Threads (sleep) where
+
+import Foreign.C.Types
+
+foreign import capi "c_extern.h chThdSleepMilliseconds"
+  c_chThdSleepMilliseconds :: CInt -> IO ()
+
+sleep :: CInt -> IO ()
+sleep = c_chThdSleepMilliseconds
